e7c70415db61cb1f36588edb5c5105d74eca0823
ErickArambula <eric.alejandro@hotmail.com>
2023-10-22 23:17:16 -0600

    2.0

---
+++ 3. Comprobacion hacia delante.py
@@ -0,0 +1,31 @@
+from constraint import Problem, AllDifferentConstraint, add_constraint
+
+# Creación de un problema CSP
+problem = Problem()
+
+# Definición de variables y sus dominios
+problem.addVariable("A", [1, 2, 3])
+problem.addVariable("B", [4, 5, 6])
+problem.addVariable("C", [7, 8, 9])
+
+# Definición de restricciones
+problem.addConstraint(lambda a, b, c: a + b == c, ("A", "B", "C"))
+problem.addConstraint(AllDifferentConstraint(), ["A", "B", "C"])
+
+# Crear una copia del problema con comprobación hacia adelante
+problem_copy = problem.copy()
+add_constraint(lambda a, b, c: a + b == c, ("A", "B", "C"), problem_copy)
+add_constraint(AllDifferentConstraint(), ["A", "B", "C"], problem_copy)
+
+# Obtener todas las soluciones
+solutions = problem.getSolutions()
+solutions_copy = problem_copy.getSolutions()
+
+# Imprimir las soluciones
+print("Soluciones sin comprobación hacia adelante:")
+for solution in solutions:
+    print(solution)
+
+print("\nSoluciones con comprobación hacia adelante:")
+for solution in solutions_copy:
+    print(solution)
